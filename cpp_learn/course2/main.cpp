5e122ad3d0500eac83d9e220c7d316d4789ec6b9
陈伟杰 <chenweijie01@corp.netease.com>
2021-03-01 21:23:44 +0800

    sync

--- cpp_learn/course2/main.cpp
+++ cpp_learn/course2/main.cpp
@@ -1,11 +1,14 @@
 #include "test1.h"
 #include "test2.h"
+#include "my_rtti.h"
 
 int main() {
 	test_data_ptr();
 	test_data_mi_ptr();
 	test_data_move();
 	test_virtual_function();
+	test_dyn();
+	test_sidecast();
 	return 0;
 }
 
